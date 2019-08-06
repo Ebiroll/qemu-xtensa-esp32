5e19602f60ec9ee631ae17e48e3ab6daffd1ede1
Ivan Grokhotkov <ivan@espressif.com>
2019-08-06 20:00:06 +0200

    WIP: LOCAL: make qemu compatible with standard GDB builds

--- target/xtensa/helper.c
+++ target/xtensa/helper.c
@@ -180,7 +180,7 @@ static void xtensa_core_class_init(ObjectClass *oc, void *data)
      * for that: reset bit 0 in the 'flags' field of the registers definitions
      * in the gdb/xtensa-config.c inside gdb source tree or inside gdb overlay.
      */
-    cc->gdb_num_core_regs = config->gdb_regmap.num_regs;
+    cc->gdb_num_core_regs = config->gdb_regmap.num_core_regs;
 }
 
 void xtensa_register_core(XtensaConfigList *node)
